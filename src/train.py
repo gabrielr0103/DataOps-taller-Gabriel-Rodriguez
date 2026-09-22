265cfaa4745106f2d3fbddb3bda6af885a96ae9e
gabrielr0103 <gabrielrp0103@gmail.com>
2026-09-22 07:36:53 -0500

    feat: add train module

---
+++ src/train.py
@@ -0,0 +1,7 @@
+"""Módulo de entrenamiento del modelo.
+
+Contendrá train_model(df), que entrena una regresión lineal para
+predecir ventas y guarda el modelo con joblib.
+Se implementará en la Tarea 2.
+"""
+
