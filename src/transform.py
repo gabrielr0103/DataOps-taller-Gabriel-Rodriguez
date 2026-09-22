92bcf6d25638afa14f1b4efff73e4cccf6d19125
gabrielr0103 <gabrielrp0103@gmail.com>
2026-09-22 07:36:28 -0500

    feat: add transform module

---
+++ src/transform.py
@@ -0,0 +1,6 @@
+"""Módulo de transformación de datos.
+
+Contendrá clean_data(df), calculate_metrics(df) y aggregate_sales(df)
+para limpiar los datos y calcular métricas de ventas.
+Se implementará en la Tarea 2.
+"""
