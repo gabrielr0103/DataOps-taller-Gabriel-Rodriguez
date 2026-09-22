57679fd9c6243631375bebc800d2da051c2f7027
gabrielr0103 <gabrielrp0103@gmail.com>
2026-09-22 07:35:26 -0500

    feat: add create_db script skeleton

---
+++ scripts/create_db.py
@@ -0,0 +1,7 @@
+"""Script de creación de la base de datos SQLite.
+
+Generará la tabla `ventas` con al menos 100 registros simulados
+(incluyendo valores nulos y duplicados) para las pruebas de calidad.
+Se implementará en la Tarea 2.
+"""
+
